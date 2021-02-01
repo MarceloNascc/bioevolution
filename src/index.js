a7a3aff699db77cc1b25b3c9f82fa9d2c9fc95f6
MarceloNascc <marcelo.nasccc@gmail.com>
2021-02-01 03:10:09 -0300

    (feat): add routes

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import Home from './pages/Home';
+import Router from './Router';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Home />
+    <Router />
   </React.StrictMode>,
   document.getElementById('root')
 );
