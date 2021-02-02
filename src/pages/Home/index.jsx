c3027d1ab2821c2c7daeabef4b8b561bbbb25c04
MarceloNascc <marcelo.nasccc@gmail.com>
2021-02-02 00:22:16 -0300

    (feat): change home layout

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,4 +1,10 @@
 import React from 'react';
+import naturalSelection from '../../assets/selecao_natural.jpg';
+import darwinismo from '../../assets/darwinismo.jpg';
+import geneticDrift from '../../assets/deriva_genetica.png';
+import speciation from '../../assets/especiacao.png';
+import lamarckismo from '../../assets/lamarckismo.jpg';
+import artificialSelection from '../../assets/selecao_artificial.jpeg';
 
 import { Link } from 'react-router-dom';
 
@@ -7,57 +13,60 @@ import './styles.css';
 export default function Home() {
   return (
     <>
-      <div className='content' id='main-content-home'>
-        <h1 className='title' id='home-title'>Assuntos</h1>
+      <div id='home-main'>
+        <div id='home-left' />
+        <div id='main-content-home'>
+          <h1 className='title' id='home-title'>Assuntos</h1>
 
-        <main id='home'>
-          <nav id='home-navigation'>
-            <ul>
-              <li>
-                <Link>
-                  <img className='link-img' src="https://www.kyb.com.br/catalogo/administracao/assets_admin/plugins/fullPage.js/examples/imgs/bg2.jpg" alt="uma imagem" />
-                  <p className='paragraph-img'>Seleção Natural</p>
-                </Link>
-              </li>
+          <main id='home'>
+            <nav>
+              <ul id='home-navigation'>
+                <li>
+                  <Link>
+                    <img className='link-img' src={naturalSelection} alt="imagem representando a seleção natural" />
+                    <p className='paragraph-img'>Seleção Natural</p>
+                  </Link>
+                </li>
 
-              <li>
-                <Link>
-                  <img className='link-img' src="https://www.kyb.com.br/catalogo/administracao/assets_admin/plugins/fullPage.js/examples/imgs/bg2.jpg" alt="uma imagem" />
-                  <p className='paragraph-img'>Seleção Artificial</p>
-                </Link>
-              </li>
+                <li>
+                  <Link>
+                    <img className='link-img' src={artificialSelection} alt="uma imagem" />
+                    <p className='paragraph-img'>Seleção Artificial</p>
+                  </Link>
+                </li>
 
-              <li>
-                <Link>
-                  <img className='link-img' src="https://www.kyb.com.br/catalogo/administracao/assets_admin/plugins/fullPage.js/examples/imgs/bg2.jpg" alt="uma imagem" />
-                  <p className='paragraph-img'>Derivação Genética</p>
-                </Link>
-              </li>
+                <li>
+                  <Link>
+                    <img className='link-img' src={geneticDrift} alt="uma imagem" />
+                    <p className='paragraph-img'>Deriva Genética</p>
+                  </Link>
+                </li>
 
-              <li>
-                <Link>
-                  <img className='link-img' src="https://www.kyb.com.br/catalogo/administracao/assets_admin/plugins/fullPage.js/examples/imgs/bg2.jpg" alt="uma imagem" />
-                  <p className='paragraph-img'>Darwinismo</p>
-                </Link>
-              </li>
+                <li>
+                  <Link>
+                    <img className='link-img' src={darwinismo} alt="uma imagem" />
+                    <p className='paragraph-img'>Darwinismo</p>
+                  </Link>
+                </li>
 
-              <li>
-                <Link>
-                  <img className='link-img' src="https://www.kyb.com.br/catalogo/administracao/assets_admin/plugins/fullPage.js/examples/imgs/bg2.jpg" alt="uma imagem" />
-                  <p className='paragraph-img'>Lamarckismo</p>
-                </Link>
-              </li>
+                <li>
+                  <Link>
+                    <img className='link-img' src={lamarckismo} alt="uma imagem" />
+                    <p className='paragraph-img'>Lamarckismo</p>
+                  </Link>
+                </li>
 
-              <li>
-                <Link>
-                  <img className='link-img' src="https://www.kyb.com.br/catalogo/administracao/assets_admin/plugins/fullPage.js/examples/imgs/bg2.jpg" alt="uma imagem" />
-                  <p className='paragraph-img'>Teoria Endossimbiótica</p>
-                </Link>
-              </li>
+                <li>
+                  <Link>
+                    <img className='link-img' src={speciation} alt="uma imagem" />
+                    <p className='paragraph-img'>Especiação</p>
+                  </Link>
+                </li>
 
-            </ul>
-          </nav>
-        </main>
+              </ul>
+            </nav>
+          </main>
+        </div>
       </div>
 
       <section className='content'>
