bc76f2458888d95a089b2d9b33e041bec005351d
MarceloNascc <marcelo.nasccc@gmail.com>
2021-02-03 03:02:11 -0300

    (feat): redirect to home by logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
-
 import logo from '../../assets/logo.png';
 import logoWhite from '../../assets/logo_white.png';
+
+import { Link } from 'react-router-dom';
+
 import './styles.css';
 
 export default function Header({ useWhite = true }) {//create context
   return (
     <header id='header'>
-      <img src={useWhite ? logoWhite : logo} alt="a small tree" />
+      <Link to='/'><img src={useWhite ? logoWhite : logo} alt="a small tree" /></Link>
       <h1 className='title' style={{ color: useWhite ? '#FFFFFF' : '#000000' }}>
         BioEvolution
       </h1>
